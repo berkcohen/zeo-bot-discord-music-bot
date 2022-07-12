2d023c311f23ec45c0eefe47974d44ea137500dc
berkcohen <94721857+berkcohen@users.noreply.github.com>
2022-07-12 21:47:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '?',
-        token: 'OTQ1NzYxMTU2OTA2MjQyMDYx.G0teY-.AnM7Rh7NYwmo9v2QU7APVFRA7ngyKPFNqy3glE',
+        token: '',
         playing: '?help'
     },
 
